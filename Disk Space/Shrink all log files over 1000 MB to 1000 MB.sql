6a4297d9b67f8d771f2b04832aab77d1123c9dd1
David Schanzer <david.schanzer@health.nsw.gov.au>
2022-11-21 14:12:24 +1100

    Add "Randomise start times for a SQL Agent job schedule to be within an hour of a given time"
    Add "Randomise start times for a SQL Agent job schedule to be within an hour of a given time", and some minor improvements to other scripts

--- Disk Space/Shrink all log files over 1000 MB to 1000 MB.sql
+++ Disk Space/Shrink all log files over 1000 MB to 1000 MB.sql
@@ -7,7 +7,7 @@ DECLARE @new_size_MB int = 1000,
 		@curr_size_8K int,
 		@sql VARCHAR(1000);
 SELECT @curr_size_8K = size,
-	   @sql = ''DBCC SHRINKFILE (N'''''' + name + '''''' , 1000)''
+	   @sql = ''DBCC SHRINKFILE (N'''''' + name + '''''', '' + CAST(@new_size_MB AS VARCHAR(10)) + '')''
 	FROM sys.database_files
 	WHERE type_desc = ''LOG'';
 IF @curr_size_8K * 8 / 1024 > @new_size_MB
