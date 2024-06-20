987a7187751d93a5add9dc7997d6e4ac6a592138
David Schanzer <david.schanzer@health.nsw.gov.au>
2024-06-20 10:57:54 +1000

    Minor corrections

--- Encryption In-Transit/Capture encryption status of all database connections using a Logon trigger.sql
+++ Encryption In-Transit/Capture encryption status of all database connections using a Logon trigger.sql
@@ -1,80 +1,67 @@
--- Capture encryption status of all database connections using a Logon trigger
--- Part of the SQL Server DBA Toolbox at https://github.com/DavidSchanzer/Sql-Server-DBA-Toolbox
--- This script creates a Logon trigger that records into table zDBAEncryptionAuditing.dbo.EncryptionAuditing all T-SQL TCP database connections except for certain exclusions.
--- It will only record one row per combination of (LoginName, DBName, HostName, ProgramName, EncryptOption), and also recording the most recent LoginTime, SPID and HostProcessID.
--- This allows you to see, for each program on a host that uses a login to make a connection to a database, when the last time was that both an encrypted and an unencrypted
--- connection was made. This enables you to confidently force encrypted connections only, knowing that the last time each program made an unencrypted connection was some time ago,
--- and that the program now makes encrypted connections.
+---- Capture encryption status of all database connections using a Logon trigger
+---- Part of the SQL Server DBA Toolbox at https://github.com/DavidSchanzer/Sql-Server-DBA-Toolbox
+---- This script creates a Logon trigger that records into table zDBAEncryptionAuditing.dbo.EncryptionAuditing all T-SQL TCP database connections except for certain exclusions.
+---- It will only record one row per combination of (LoginName, DBName, HostName, ProgramName, EncryptOption), and also recording the most recent LoginTime, SPID and HostProcessID.
+---- This allows you to see, for each program on a host that uses a login to make a connection to a database, when the last time was that both an encrypted and an unencrypted
+---- connection was made. This enables you to confidently force encrypted connections only, knowing that the last time each program made an unencrypted connection was some time ago,
+---- and that the program now makes encrypted connections.
 
-USE master
-GO
+--USE master
+--GO
 
--- Create the zDBAEncryptionAuditing database, dropping it if it already existed
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'zDBAEncryptionAuditing')
-BEGIN
-	ALTER DATABASE zDBAEncryptionAuditing SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE IF EXISTS zDBAEncryptionAuditing;
-END
-GO
-CREATE DATABASE zDBAEncryptionAuditing;
-GO
+---- Create the zDBAEncryptionAuditing database, dropping it if it already existed
+--IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'zDBAEncryptionAuditing')
+--BEGIN
+--	ALTER DATABASE zDBAEncryptionAuditing SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+--	DROP DATABASE IF EXISTS zDBAEncryptionAuditing;
+--END
+--GO
+--CREATE DATABASE zDBAEncryptionAuditing;
+--GO
 
--- Turn on RCSI, as without this you can get the error:
--- "Snapshot isolation transaction failed accessing database 'zDBAEncryptionAuditing' because snapshot isolation is not allowed in this database. Use ALTER DATABASE to allow snapshot isolation."
-ALTER DATABASE zDBAEncryptionAuditing SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-ALTER DATABASE zDBAEncryptionAuditing SET READ_COMMITTED_SNAPSHOT ON;
-ALTER DATABASE zDBAEncryptionAuditing SET MULTI_USER;
-GO
+---- Turn on RCSI, as without this you can get the error:
+---- "Snapshot isolation transaction failed accessing database 'zDBAEncryptionAuditing' because snapshot isolation is not allowed in this database. Use ALTER DATABASE to allow snapshot isolation."
+--ALTER DATABASE zDBAEncryptionAuditing SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+--ALTER DATABASE zDBAEncryptionAuditing SET READ_COMMITTED_SNAPSHOT ON;
+--ALTER DATABASE zDBAEncryptionAuditing SET ALLOW_SNAPSHOT_ISOLATION ON;
+--ALTER DATABASE zDBAEncryptionAuditing SET MULTI_USER;
+--GO
 
--- Grant permission for any login to query sys.dm_exec_sessions and sys.dm_exec_connections
-IF CAST(CAST(SERVERPROPERTY('productversion') AS CHAR(2)) AS TINYINT) >= 16		-- SQL Server 2022 or above
-	EXEC('GRANT VIEW SERVER PERFORMANCE STATE TO [public];');
-ELSE
-	EXEC('GRANT VIEW SERVER STATE TO [public];');
-GO
+--USE zDBAEncryptionAuditing;
+--GO
 
-USE zDBAEncryptionAuditing;
-GO
+--CREATE TABLE dbo.EncryptionAuditing
+--(
+--    [LoginName] VARCHAR(128) NOT NULL,
+--    [DBName] VARCHAR(128) NOT NULL,
+--    [HostName] VARCHAR(128) NOT NULL,
+--    [ProgramName] VARCHAR(128) NOT NULL,
+--    [EncryptOption] CHAR(5) NOT NULL,
+--    [MostRecentLoginTime] VARCHAR(128) NULL,
+--    [MostRecentSPID] INT NULL,
+--    [MostRecentHostProcessID] INT NULL
+--);
 
--- Set the database owner to sa
-ALTER AUTHORIZATION ON DATABASE::[zDBAEncryptionAuditing] TO [sa]
-GO
+---- Create the PK on the combination of columns that we want to be unique, to limit the number of rows in this table
+--ALTER TABLE dbo.EncryptionAuditing
+--ADD CONSTRAINT PK_EncryptionAuditing
+--    PRIMARY KEY CLUSTERED (
+--                              LoginName,
+--                              DBName,
+--                              HostName,
+--                              ProgramName,
+--                              EncryptOption
+--                          );
+--GO
 
--- Grant all logins the ability to insert into the table dbo.EncryptionAuditing, by enabling the guest user
-GRANT CONNECT, INSERT, UPDATE, SELECT TO [guest];
-GO
-
-CREATE TABLE dbo.EncryptionAuditing
-(
-    [LoginName] VARCHAR(128) NOT NULL,
-    [DBName] VARCHAR(128) NOT NULL,
-    [HostName] VARCHAR(128) NOT NULL,
-    [ProgramName] VARCHAR(128) NOT NULL,
-    [EncryptOption] CHAR(5) NOT NULL,
-    [MostRecentLoginTime] VARCHAR(128) NULL,
-    [MostRecentSPID] INT NULL,
-    [MostRecentHostProcessID] INT NULL
-);
-
--- Create the PK on the combination of columns that we want to be unique, to limit the number of rows in this table
-ALTER TABLE dbo.EncryptionAuditing
-ADD CONSTRAINT PK_EncryptionAuditing
-    PRIMARY KEY CLUSTERED (
-                              LoginName,
-                              DBName,
-                              HostName,
-                              ProgramName,
-                              EncryptOption
-                          );
-GO
-
-DROP TRIGGER IF EXISTS TR_zDBAEncryptionAuditing
-ON ALL SERVER;
-GO
+--DROP TRIGGER IF EXISTS TR_zDBAEncryptionAuditing
+--ON ALL SERVER;
+--GO
 
 -- Create the logon trigger
 CREATE OR ALTER TRIGGER TR_zDBAEncryptionAuditing
 ON ALL SERVER
+WITH EXECUTE AS N'sa'
 FOR LOGON
 AS
 DECLARE @EventData XML,
@@ -86,7 +73,7 @@ DECLARE @EventData XML,
         @HostName NVARCHAR(128),
         @ProgramName NVARCHAR(128),
         @HostProcessID INT,
-		@ProtocolType NVARCHAR(40);
+		@NetTransport NVARCHAR(40);
 
 -- Create the table if it doesn't already exist - this is just in case someone drops the table, which might cause all login attempts to fail.
 IF NOT EXISTS
@@ -132,12 +119,12 @@ SELECT @DBName = ISNULL(DB_NAME(database_id), ''),
 FROM sys.dm_exec_sessions
 WHERE session_id = @SPID;
 
-SELECT @EncryptOption = ISNULL(encrypt_option, ''), @ProtocolType = protocol_type
+SELECT @EncryptOption = ISNULL(encrypt_option, ''), @NetTransport = net_transport
 FROM sys.dm_exec_connections
 WHERE session_id = @SPID;
 
 -- Don't bother inserting a row for certain values of ProgramName, LoginName and DBName, as these come from applications that we don't need to track.
-IF @ProtocolType = 'TSQL'
+IF @NetTransport = 'TCP'
    AND @ProgramName NOT LIKE 'SQLAgent%'
    AND @ProgramName NOT LIKE 'Microsoft SQL Server%'
    AND @ProgramName NOT LIKE 'Red Gate Software%'
